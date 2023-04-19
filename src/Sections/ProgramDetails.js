b18b7b016226caa79affec205de978f1f128bc3c
Taimur Ahmad <50269907+Taimur-DevOps@users.noreply.github.com>
2023-04-20 03:51:28 +0500

    Create ProgramDetails.js
    ProgramDetails Structure

---
+++ src/Sections/ProgramDetails.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ProgramDetails = () => {
+  return (
+    <div className="  bg-primary">
+    <div className="max-w-[1138px] h-[641px] mx-auto  ">
+      <div className="flex flex-col gap-[36px] flex-start">
+      <div className="flex items-center justify-center py-[30px]"> 
+        <h1 className="text-[40px] leading-[60px] font-semibold">Who is this <span className='text-white underline'> Program   </span> for?</h1>
+      </div>
+      <div className="flex flex-col h-[384px]">
+        
+       
+       
+      </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default ProgramDetails
