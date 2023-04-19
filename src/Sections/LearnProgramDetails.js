7c3735d10aa3d341755e076c8ae35bc292c91d9d
Taimur Ahmad <50269907+Taimur-DevOps@users.noreply.github.com>
2023-04-20 00:59:03 +0500

    Update LearnProgramDetails.js

--- src/Sections/LearnProgramDetails.js
+++ src/Sections/LearnProgramDetails.js
@@ -1,42 +1,44 @@
 import React from 'react'
 
 const LearnProgramDetails = () => {
+
+const cards=[
+  {id:0,title:"The Program allows you to develop essential leadership skills for effective team management and organizational success."},
+  {id:1,title:"Through the Program, you will gain expertise in strategic planning to set goals, make informed decisions, and drive business growth."},
+  {id:2,title:"Learn how to build a positive business culture through the Program, fostering collaboration, innovation, and inclusivity."},
+  {id:3,title:"The Program provides insights into inclusive recruitment strategies for creating a diverse and motivated workforces."},
+  {id:4,title:"Acquire knowledge of sustainable marketing practices through the Program, focusing on long-term environmental and social impact."},
+  {id:5,title:"Learn meaningful customer engagement techniques in the Program to build strong customer relationships and drive loyalty."},
+  {id:6,title:"Gain a comprehensive understanding of business fundamentals, including accounting, finance, marketing, operations, and human resources, through the Program."},
+  
+]
+
   return (
     <div className="  bg-slate-100">
-    <div className="max-w-[1138px] h-[641px] mx-auto  ">
-      <div className="flex flex-col gap-[36px] flex-start">
-      <div className="flex items-center justify-center py-[30px]"> 
-        <h1 className="text-[40px] leading-[60px] font-semibold">What will you learn in this Program?</h1>
-      </div>
-      <div className="flex flex-col h-[384px]">
-        
-        <p className="text-[20px] leading-[32px]  text-justify">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          mollis rhoncus dolor, id tempor est tincidunt ac. Integer semper
-          metus at convallis mollis. Maecenas non dui lectus. Vestibulum
-          commodo quam vel molestie ornare. Etiam auctor, felis quis ultrices
-          iaculis, augue metus pellentesque ligula, eget dignissim lacus
-          tellus id neque. Ut pharetra lacus vitae rutrum ultrices. Morbi
-          facilisis risus eget convallis pulvinar. Aliquam ac commodo ex.
-          Aliquam sed posuere libero. Sed efficitur magna at malesuada
-          placerat. Nulla interdum justo sed erat pulvinar, vitae scelerisque
-          velit faucibus. Donec ac augue dictum, luctus lorem quis, bibendum
-          urna. Proin quis justo nec velit tincidunt mattis sed in dui. Cras
-          sagittis, dolor in luctus eleifend, sem lorem volutpat lorem, non
-          blandit mauris metus quis nibh. Aliquam erat volutpat. In hac
-          habitasse platea dictumst. Sed facilisis ante sit amet aliquet
-          porttitor. Quisque varius vehicula tincidunt. Maecenas et dui
-          aliquet, malesuada tellus at, lobortis massa. Aliquam lacinia risus
-          et ex volutpat, non porta mi volutpat. Etiam sapien dolor, interdum
-          nec felis ut, rhoncus tristique nibh. In in purus eleifend, volutpat
-          ante in, bibendum lectus. Duis non nisl non nulla iaculis
-          condimentum at vitae elit.
-        </p>
+      <div className="max-w-[1138px]  mx-auto  ">
+        <div className="flex flex-col  flex-start">
+          <div className="flex items-center justify-center py-[30px]">
+            <h1 className="text-[40px] leading-[60px] font-semibold">What will you learn in this Program?</h1>
+          </div>
+
+       {   cards.map((item)=>{
+            return(
+              <div className='w-full border bg-[#F05A221C] border-primary px-[45px] pt-[24px] pb-[29px] rounded-[10px] flex gap-[18px] items-center mb-[24px]'>
+              <span>icon</span>
+              <p className='text-[22px] leading-[30px] font-semibold '>{item.title}</p>
+            </div>
+            );
+          })
+        }
+         
+
+          
+
+
        
-      </div>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
